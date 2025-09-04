15d3ce45f14b943034fe97367ad3081c35a48f40
jtuluve <jtuluve@gmail.com>
2025-09-04 10:30:43 +0530

    Updated data with images

--- constants/data.ts
+++ constants/data.ts
@@ -1,18 +1,18 @@
 export const projects = [
   {
-    image: "/images/genasiri.png",
+    image: "/projects/genasiri.png",
     title: "Genasiri",
     description:
-      "A transliterator enabling 1000+ users to convert text from multiple languages into Tulu script.",
+      "A transliteration tool that enables 1000+ users to seamlessly convert text from multiple languages into Tulu script, making Tulu more accessible in the digital space.",
     technologies: ["HTML", "CSS", "JavaScript"],
-    liveUrl: "#",
-    codeUrl: "#",
+    liveUrl: "https://jaitulunad.in/others/genasiri",
+    codeUrl: "https://github.com/jtuluve/Genasiri-Tulu-Transliteration",
   },
   {
-    image: "/images/musyncc.png",
+    image: "/projects/musyncc.png",
     title: "Musyncc",
     description:
-      "Built a real-time music synchronization platform using WebSockets and the YouTube API, enabling users to listen to music together seamlessly.",
+      "A real-time music synchronization platform that uses WebSockets and the YouTube API to let users listen to music together, creating a shared listening experience.",
     technologies: [
       "React",
       "Next.js",
@@ -22,14 +22,14 @@ export const projects = [
       "Node.js",
       "Tailwind CSS",
     ],
-    liveUrl: "#",
-    codeUrl: "#",
+    liveUrl: "https://musyncc.vercel.app",
+    codeUrl: "https://github.com/jtuluve/musyncc",
   },
   {
-    image: "/images/ezorder.png",
+    image: "/projects/ezorder.png",
     title: "EZOrder",
     description:
-      "Built a user-friendly web app enabling instant food ordering with 24-hour order validity and seamless in-person payment.",
+      "A modern food-ordering web app offering instant order placement with 24-hour order validity and in-person payment support. Optimized for speed, scalability, and reliability.",
     technologies: [
       "React",
       "WebSocket",
@@ -38,9 +38,127 @@ export const projects = [
       "Express.js",
       "MongoDB",
       "Node.js",
+      "Redis",
+      "Docker",
     ],
-    liveUrl: "#",
-    codeUrl: "#",
+    liveUrl: "https://ez-order-three.vercel.app",
+    codeUrl: "https://github.com/jtuluve/EZOrder",
+  },
+  {
+    image: "/projects/hospital.png",
+    title: "Centralized Hospital Resource Management System",
+    description:
+      "A modular, real-time hospital management system to efficiently coordinate between departments like Emergency Alerts, Token Queue, Pharmacy Inventory, and Blood Bank — all powered by role-based access and WebSockets.",
+    technologies: [
+      "React",
+      "Vite",
+      "Tailwind",
+      "Node.js",
+      "Express",
+      "Socket.IO",
+      "PostgreSQL",
+      "Prisma ORM",
+    ],
+    liveUrl: "https://hospital-management-system2.vercel.app/",
+    codeUrl: "https://github.com/jtuluve/hospital-management-system",
+  },
+  {
+    image: "/projects/create-express-doc.png",
+    title: "Express Documentation Generator (create-express-doc)",
+    description:
+      "An npx CLI tool that automatically generates comprehensive API documentation for Express.js applications by analyzing your code. Simplifies the process of keeping API docs up-to-date.",
+    technologies: ["JavaScript", "Babel", "Node.js"],
+    liveUrl: "https://www.npmjs.com/package/create-express-doc",
+    codeUrl: "https://github.com/jtuluve/create-express-doc",
+  },
+  {
+    image: "/projects/litekv.png",
+    title: "LiteKV",
+    description:
+      "A lightweight TypeScript/JavaScript client library for LiteKV key-value storage service. Provides persistent storage with simple APIs, designed for scalability and ease of use.",
+    technologies: ["TypeScript", "JavaScript"],
+    liveUrl: "https://www.npmjs.com/package/litekv",
+    codeUrl: "https://github.com/jtuluve/litekv",
+  },
+  {
+    image: "/projects/litekv-api.png",
+    title: "LiteKV-api",
+    description:
+      "A api providing persistent key value storage service for applications. Built using Nest.js, MongoDB, and Docker, it offers a scalable and reliable solution for storing and retrieving data.",
+    technologies: ["TypeScript", "JavaScript", "Nest.js", "MongoDB", "Docker"],
+    liveUrl: "https://litekv-api.onrender.com",
+    codeUrl: "https://github.com/jtuluve/litekv-api",
+  },
+  {
+    image: "/projects/aniportal.png",
+    title: "AniPortal",
+    description:
+      "AniPortal is a web application for browsing and managing anime information. Users can search for anime, view details, and manage their own anime lists.",
+    technologies: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "MongoDB",
+      "Mongoose",
+      "Next-Auth",
+      "Tailwind CSS",
+    ],
+    liveUrl: "https://aniportal.vercel.app",
+    codeUrl: "https://github.com/jtuluve/aniportal",
+  },
+  {
+    image: "/projects/telebaravu.png",
+    title: "Telebaravu",
+    description:
+      "A Telegram bot that generates PNG images in Tulu script from Kannada, Malayalam, or Hindi text inputs. Generated over 1000 images.",
+    technologies: ["grammY", "JavaScript", "TypeScript", "Express.js"],
+    liveUrl: "https://telegram.me/TelebaravuBot",
+    codeUrl: "https://github.com/jtuluve/Telebaravu",
+  },
+  {
+    image: "/projects/robinsan.png",
+    title: "🤖 Robin-San Bot",
+    description:
+      "A feature-rich, anime-centric Telegram bot in beta. Provides the latest anime information, including top airing shows, popular titles, and seasonal recommendations.",
+    technologies: ["JavaScript", "Telegraf.js", "External APIs"],
+    liveUrl: "https://telegram.me/RobinSanBot",
+    codeUrl: "https://github.com/jtuluve/RobinSan-bot",
+  },
+  {
+    image: "/projects/anywhere-tulu.png",
+    title: "Anywhere Tulu",
+    description:
+      "A lightweight web tool that lets developers transliterate Kannada, Malayalam, or Hindi text into Tulu script directly within web applications. Powered by Genasiri’s transliteration engine.",
+    technologies: ["JavaScript"],
+    liveUrl: "https://jtuluve.github.io/anywhere-tulu/example.html",
+    codeUrl: "https://github.com/jtuluve/anywhere-tulu",
+  },
+  {
+    image: "/projects/emage.png",
+    title: "Emage",
+    description:
+      "A creative web application that transforms images into emoji mosaics. Upload an image and instantly get a unique emoji-based representation—all in your browser.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    liveUrl: "https://emage-jtuluve.vercel.app",
+    codeUrl: "https://github.com/jtuluve/emage",
+  },
+  {
+    image: "/projects/two-cars.png",
+    title: "Two Cars Game",
+    description:
+      "A fast-paced reflex game where players control two cars simultaneously, avoiding obstacles and collecting points. Lightweight, responsive, and fun for casual gameplay.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    liveUrl: "https://two-cars.vercel.app/",
+    codeUrl: "https://github.com/jtuluve/two-cars",
+  },
+  {
+    image: "/projects/snake.png",
+    title: "That Snake Again",
+    description:
+      "A modern take on the classic Snake game built with HTML, CSS, and JavaScript. Simple, nostalgic, and addictive gameplay experience.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    liveUrl: "https://jtuluve.github.io/that-snake-again/",
+    codeUrl: "https://github.com/jtuluve/that-snake-again",
   },
 ];
 
@@ -64,6 +182,7 @@ export const skillCategories = [
       { name: "Socket.IO", iconSrc: "/icons/socket-io.png" },
       { name: "Puppeteer", iconSrc: "/icons/puppeteer.png" },
       { name: "Auth.js", iconSrc: "/icons/authjs.png" },
+      { name: "Prisma ORM", iconSrc: "/icons/prisma.png" },
     ],
   },
   {
@@ -83,7 +202,6 @@ export const skillCategories = [
       { name: "SQL", iconSrc: "/icons/sql.png" },
       { name: "Redis", iconSrc: "/icons/redis.png" },
       { name: "Supabase", iconSrc: "/icons/supabase.png" },
-      { name: "Prisma ORM", iconSrc: "/icons/prisma.png" },
     ],
   },
   {
@@ -92,6 +210,7 @@ export const skillCategories = [
       { name: "Git", iconSrc: "/icons/git.png" },
       { name: "GitHub", iconSrc: "/icons/github.png" },
       { name: "Docker", iconSrc: "/icons/docker.png" },
+      { name: "UiPath", iconSrc: "/icons/uipath.png" },
     ],
   },
   {
@@ -105,35 +224,37 @@ export const skillCategories = [
   },
 ];
 
-
 export const experience = [
   {
     role: "Full Stack Developer Intern",
     company: "A J Institute of Medical Sciences and Research Center, Mangaluru",
     duration: "Sept 2023 - Aug 2024",
-    shortDesc: "Developed a faculty appraisal management web application using modern web technologies.",
+    shortDesc:
+      "Developed a faculty appraisal management web application using modern web technologies.",
     points: [
-      "Developed 'Faculty Appraisal Forum' software, a web application utilizing React, Next.js, Auth.js, MongoDB, and Firebase Storage."
-    ]
+      "Developed 'Faculty Appraisal Forum' software, a web application utilizing React, Next.js, Auth.js, MongoDB, and Firebase Storage.",
+    ],
   },
   {
     role: "Freelance Web Developer",
     company: "Kreekarvat Technologies, Mangaluru",
     duration: "Nov 2024 - Mar 2025",
-    shortDesc: "Built responsive websites and implemented automation solutions for client operations.",
+    shortDesc:
+      "Built 2 responsive websites for SVST institutions using modern web technologies, attracting 5,000+ visitors and enhancing digital presence.",
     points: [
       "Built 2 responsive websites for SVST institutions using modern web technologies, attracting 5,000+ visitors and enhancing digital presence.",
-      "Used Nodemailer, Gmail API, and UIPath automation tool for emailing and event triggers."
-    ]
+      "Used Nodemailer, Gmail API, and UIPath automation tool for emailing and event triggers.",
+    ],
   },
   {
     role: "Freelance Software Developer",
     company: "BillMaxo Solutions, Bengaluru",
     duration: "Jan 2025 - Jun 2025",
-    shortDesc: "Developed automation tools and APIs for WhatsApp marketing and food delivery integrations.",
+    shortDesc:
+      "Developed automation tools and APIs for WhatsApp marketing and food delivery integrations.",
     points: [
       "Developed a WhatsApp Marketing API using Express.js, Puppeteer, and whatsapp-web.js.",
-      "Built Swiggy and Zomato integration tools for automated order scraping and restaurant data insights."
-    ]
-  }
+      "Built Swiggy and Zomato integration tools for automated order scraping and restaurant data insights.",
+    ],
+  },
 ];
